ee9c002e471722984c83c27b2f94c302496a63d0
Arik775 <bread9765@gmail.com>
2022-03-22 16:51:30 +0700

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TestInputComponent } from './test-input/test-input.component';
+import { TestSelectComponent } from './test-select/test-select.component';
+import { TestNumberComponent } from './test-number/test-number.component';
+import { TestCheckboxComponent } from './test-checkbox/test-checkbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestInputComponent,
+    TestSelectComponent,
+    TestNumberComponent,
+    TestCheckboxComponent
   ],
   imports: [
     BrowserModule
