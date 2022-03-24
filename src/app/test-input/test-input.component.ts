d5a0c6554d0ca56e46e70bd62c7793554d3b7e06
Arik775 <bread9765@gmail.com>
2022-03-24 18:57:00 +0700

    Fully finished front

--- src/app/test-input/test-input.component.ts
+++ src/app/test-input/test-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test-input',
@@ -9,7 +9,12 @@ export class TestInputComponent implements OnInit {
 
   constructor() { }
 
+  @Input() textComp: string = 'Имя'
+
+
+
   ngOnInit(): void {
+
   }
 
 }
